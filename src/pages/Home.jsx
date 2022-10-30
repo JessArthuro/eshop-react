9190844662b38b9a85fdbae47eb614ea5fb4ab5d
Jesus Arturo Carmona Aguirre <jessarturo97@gmail.com>
2022-10-30 10:36:13 -0600

    Se agrego contenido en components, containers, pages, routes y styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import Header from '../components/Header'
+import React from "react";
+import ProductList from "@containers/ProductList";
 
 function Home() {
   return (
-    <Header />
-  )
+    <>
+      <ProductList />
+    </>
+  );
 }
 
 export default Home;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,23 @@
 import React from "react";
-import "../styles/Login.scss";
+import "@styles/Login.scss";
+import logo from "@logos/logo_yard_sale.svg";
 
 function Login() {
   return (
-    <div className="login">
-      <div className="form-container">
-        <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
-
-        <h1 className="title">Create a new password</h1>
-        <p className="subtitle">Enter a new passwrd for yue account</p>
-
+    <div className="Login">
+      <div className="Login-container">
+        <img src={logo} alt="logo" className="logo" />
         <form action="/" className="form">
-          <label for="password" className="label">
+          <label htmlFor="email" className="label">
+            Email address
+          </label>
+          <input
+            type="text"
+            id="email"
+            placeholder="platzi@example.cm"
+            className="input input-email"
+          />
+          <label htmlFor="password" className="label">
             Password
           </label>
           <input
@@ -20,23 +26,14 @@ function Login() {
             placeholder="*********"
             className="input input-password"
           />
-
-          <label for="new-password" className="label">
-            Password
-          </label>
-          <input
-            type="password"
-            id="new-password"
-            placeholder="*********"
-            className="input input-password"
-          />
-
           <input
             type="submit"
-            value="Confirm"
+            value="Log in"
             className="primary-button login-button"
           />
+          <a href="/">Forgot my password</a>
         </form>
+        <button className="secondary-button signup-button">Sign up</button>
       </div>
     </div>
   );
